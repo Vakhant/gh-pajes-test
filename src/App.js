e1dab0470030fb45a358f09f9a49793f55e06564
VirSpectabilis <rinaldo124@mail.ru>
2022-03-15 17:03:58 +0300

    main: test axios cors gh-pages error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import * as axios from "axios";
+
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "2fc274f6-5c86-48e9-aa6f-8e920b316173",
+    }
+})
+
+const getUsers = (currentPage = 1, pageSize = 10) => {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => {
+            console.log(response.data);
+        })
+}
 
 function App() {
+  getUsers()
   return (
     <div className="App">
       <header className="App-header">
@@ -16,6 +33,11 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <p>
+            gh-pages-test2
+          <br/>
+            rebuild-test
+          </p>
         </a>
       </header>
     </div>
